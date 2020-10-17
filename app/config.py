69fcb23ecadcc8dc6454d152034e0437aee52cfe
miguelVar <vargas.miguel32@gmail.com>
2020-10-16 21:34:52 -0500

    conexion a bd

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
 """ Configuration file for development"""
 import os
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'develop')
+SERVER_DATABASE = os.getenv('SERVER_DATABASE', 'localhost')
+USER_DATABASE = os.getenv('USER_DATABASE', 'root')
+PASSWORD_DATABASE = os.getenv('PASSWORD_DATABASE', 'root')
+PORT_DATABASE = os.getenv('PORT_DATABASE', '3306')
+NAME_DATABASE = os.getenv('NAME_DATABASE', 'rutinas')
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from werkzeug import exceptions
 from flask_sqlalchemy import SQLAlchemy
+from app import config
 
 APP = Flask(__name__, instance_relative_config=True)
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/rutinas'
+#APP.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/rutinas'
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+config.USER_DATABASE+':' +\
+                                        config.PASSWORD_DATABASE+'@'+config.SERVER_DATABASE +\
+                                        ':'+config.PORT_DATABASE+'/'+config.NAME_DATABASE
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(APP)
 
